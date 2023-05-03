5da871677bdcbc3be40ee38411204e2095d94e70
Ирина <Smolenskaya2002@yandex.ru>
2023-05-03 15:03:49 +0300

    Update exersize.sql

--- exersize.sql
+++ exersize.sql
@@ -23,3 +23,18 @@
  
  SELECT a.aircraft_code, a.model, s.seat_no, s.fare_conditions FROM seats s, aircrafts a WHERE s.aircraft_code = a.aircraft_code AND a.model ~ '^Cessna' ORDER BY s.seat_no;
  
+CREATE OR REPLACE VIEW flights_v AS
+SELECT f.flight_id, f.flight_no, f.scheduled_departure, timezone( dep.timezone, f.scheduled_departure ) AS scheduled_departure_local, f.scheduled_arrival, timezone( arr.timezone, f.scheduled_arrival) as scheduled_arrival_local, f.scheduled_arrival - f.scheduled_departure as scheduled_duration, f.departure_airport, dep.airport_name as departure_airport_name, dep.city as departure_city, f.arrival_airport, arr.airport_name as arrival_airport_name, arr.city as arrival_city, f.status, f.aircraft_code, f.actual_departure, timezone(dep.timezone, f.actual_departure) as actual_departure_local, f.actual_arrival, timezone(arr.timezone, f.actual_arrival) as actual_arrival_local, f.actual_arrival - f.actual_departure AS actual_duration
+FROM flights f,
+airports dep,
+airports arr
+WHERE f.departure_airport = dep.airport_code
+AND f.arrival_airport = arr.airport_code;
+ 
+SELECT count( * ) FROM airports dep, airports arr WHERE dep.city <> arr.city;
+
+SELECT r.aircraft_code, a.model, count( * ) AS num_routes
+FROM routes r
+JOIN aircrafts a ON r.aircraft_code = a.aircraft_code
+GROUP BY 1, 2
+ORDER BY 3 DESC;
